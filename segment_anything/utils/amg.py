c645c2a2ab7458f7521412a7eb2baeb19aafaa85
AYESDIE <34449856+AYESDIE@users.noreply.github.com>
2024-08-12 10:38:56 +0200

    Update amg.py

--- segment_anything/utils/amg.py
+++ segment_anything/utils/amg.py
@@ -46,7 +46,7 @@ class MaskData:
             if v is None:
                 self._stats[k] = None
             elif isinstance(v, torch.Tensor):
-                self._stats[k] = v[torch.as_tensor(keep, device='cuda:1')]
+                self._stats[k] = v[torch.as_tensor(keep, device=v.device)]
             elif isinstance(v, np.ndarray):
                 self._stats[k] = v[keep.detach().cpu().numpy()]
             elif isinstance(v, list) and keep.dtype == torch.bool:
